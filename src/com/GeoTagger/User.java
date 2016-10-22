e045e1275270845262930a6327f3a0cc33205380
Raymond Romero <rera121989@yahoo.com>
2016-10-22 14:49:34 -0700

    User Class
    Added User Class that includes username and password

---
+++ src/com/GeoTagger/User.java
@@ -0,0 +1,42 @@
+package com.GeoTagger;
+
+public class User {
+
+	int userId;
+	String username;
+	String password;
+	boolean isAdmin;
+
+	public User(int userId, String username, String password, boolean isAdmin) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
