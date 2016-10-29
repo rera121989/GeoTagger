e33a1e2c44fa4f7bb841f13226cdf9017ebe8191
Adam Sornoso <asornoso@yahoo.com>
2016-10-29 14:30:37 -0700

    request/response wrappers

---
+++ src/com/GeoTagger/GTMDRequest.java
@@ -0,0 +1,15 @@
+package com.GeoTagger;
+
+public class GTMDRequest {
+
+	/*
+	This class is a wrapper for the type of request that is received on the server and the data that is contained within the request
+	i.e.
+	 	Request{
+	 		type: db_request,
+	 		request:{
+	 			getMyTags
+	 			}
+	
+	*/
+}
---
+++ src/com/GeoTagger/GTMDResponse.java
@@ -0,0 +1,17 @@
+package com.GeoTagger;
+
+public class GTMDResponse {
+	
+	/*
+	This class is a wrapper for the type of response that is received on the server and the data that is to be sent to the client
+	i.e.
+	 	Response{
+	 		type: db_request,
+	 		response:{
+	 			MyTags:[{song name, song artist, location}, ....]}]
+	 			}
+	
+	
+	
+	*/
+}
