f12635723db8a8d3853a44db1e4c62905abc4044
Raymond Romero <rera121989@yahoo.com>
2016-10-29 14:43:33 -0700

    Local Tags
    removed pre-build comments

--- src/com/GeoTagger/LocalTags.java
+++ src/com/GeoTagger/LocalTags.java
@@ -11,23 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/tags/local")
 public class LocalTags extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
     public LocalTags() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-	
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
